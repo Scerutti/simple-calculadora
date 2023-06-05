ca51e7936ebeff7184f40524e3e93da52b06156f
Sebastián Cerutti <softsys95@gmail.com>
2023-06-04 22:37:50 -0300

    Refactoriza y mejora la calculadora
    - Agrega funcionalidad de coma decimal y borrado con tecla
    - Implementa historial de cálculos y cambio de tema
    - Aplica estilos responsivos para adaptarse a diferentes pantallas
    
    Se han realizado cambios en el HTML, CSS y JavaScript para mejorar la calculadora. Se agregó la capacidad de ingresar números con coma decimal y borrar con la tecla de retroceso. Se implementó un historial de cálculos y se añadió la función de cambio de tema (claro y oscuro). Además, se hicieron ajustes en los estilos para que la calculadora se vea bien en diferentes dispositivos y pantallas.
    
    Closes #1

---
+++ Readme.md
@@ -0,0 +1,37 @@
+# Calculadora básica
+
+Esta calculadora básica ha sido creada como proyecto personal para practicar y aplicar los conocimientos de HTML, CSS y JavaScript. Proporciona una interfaz sencilla y funcional para realizar operaciones matemáticas simples.
+
+## Características
+
+- Ingreso de números y operadores mediante botones o teclado
+- Operaciones de suma, resta, multiplicación y división
+- Funcionalidad de coma decimal y borrado de dígitos
+- Historial de cálculos realizados
+- Cambio de tema entre modo claro y oscuro
+- Diseño responsive para adaptarse a diferentes dispositivos y pantallas
+
+## Tecnologías utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Uso
+
+1. Clona este repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador web.
+3. Utiliza los botones de la calculadora o el teclado para realizar cálculos.
+4. Experimenta con las diferentes funcionalidades y cambia el tema según tus preferencias.
+
+## Contribuciones
+
+Este proyecto ha sido creado con fines educativos y de práctica personal. Por lo tanto, no se aceptarán contribuciones en este momento.
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT. Puedes consultar el archivo [LICENSE](LICENSE) para obtener más información.
+
+---
+
+Creado por [Seba Cerutti](https://sebastiancerutti.tech)
---
+++ index.js
@@ -0,0 +1,59 @@
+document.onkeydown = function (event) {
+    var key = event.key;
+    var validKeys = /[0-9\+\-\*\/\=\.]|Backspace|Delete/; // Expresión regular para validar las teclas válidas
+
+    if (validKeys.test(key)) {
+        event.preventDefault(); // Evita el comportamiento predeterminado de la tecla
+
+        if (key >= '0' && key <= '9') {
+            appendNumber(parseInt(key));
+        } else if (key === '.' && !document.getElementById('result').value.includes('.')) {
+            appendNumber('.');
+        } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+            appendOperator(key);
+        } else if (key === 'Backspace' || key === 'Delete') {
+            clearLastCharacter();
+        }
+    }
+};
+
+document.onkeyup = function (event) {
+    var key = event.key;
+
+    if (key === 'Enter' || key === '=') {
+        calculate();
+    }
+};
+
+function appendNumber(number) {
+    document.getElementById('result').value += number;
+}
+
+function appendOperator(operator) {
+    document.getElementById('result').value += operator;
+}
+
+function calculate() {
+    var expression = document.getElementById('result').value;
+    var result = eval(expression);
+    document.getElementById('result').value = result;
+
+    var historyContainer = document.getElementById('history');
+    var historyItem = document.createElement('div');
+    historyItem.textContent = expression + ' = ' + result;
+    historyContainer.appendChild(historyItem);
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function clearLastCharacter() {
+    var result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function toggleTheme() {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme');
+}
